4fa7cb147047fa11b9af21fbcd8b567a0d5a0816
raed <raed.da3k@gmail.com>
2023-08-11 11:28:24 +0100

    first test

---
+++ tests/test_console.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Testing the console """
+import unittest
+from unittest.mock import patch
+import models.engine.file_storage
+import io
+from console import HBNBCommand
+
+
+class test_console(unittest.TestCase):
+    """ testing console """
+
+    def test_create_BaseModel(self):
+        """ test create for BaseModel"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+        res = f.getvalue()
+
+        if res is not None:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
+
+    def test_create_User(self):
+        """ test create for User """
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("create User")
+        res = f.getvalue()
+
+        if res is not None:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
